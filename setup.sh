cdedddebee386c43cb6984cdd43428f2d7662174
Yinfei Yang <yangyin7@gmail.com>
2016-02-16 09:42:25 -0800

    add brew into setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,11 @@
 if test `which apt-get` ; then
     sudo apt-get -y update
     sudo apt-get -y install ctags
-if test `which yum` ; then
+    sudo apt-get -y install python
+elif test `which brew` ; then
+    brew install ctags
+    brew install python
+elif test `which yum` ; then
     sudo yum -y install ctags
+    sudo yum -y install python
+fi
