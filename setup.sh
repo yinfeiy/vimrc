52352c64e449582168be61daceace9983b00e68a
yinfeiy <yinfeiy@gmail.com>
2016-02-17 18:56:14 +0000

    setup

--- setup.sh
+++ setup.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/bash
+BASEDIR=$(dirname $0)
 
 if test `which apt-get` ; then
     sudo apt-get -y update
     sudo apt-get -y install ctags
-    sudo apt-get -y install python
+    sudo apt-get -y install tmux
 elif test `which brew` ; then
     brew install ctags
-    brew install python
+    brew install tmux
 elif test `which yum` ; then
     sudo yum -y install ctags
-    sudo yum -y install python
+    sudo yum -y install tmux
 fi
+
+cp $BASEDIR/.vimrc ~/
+cp $BASEDIR/.tmux.conf ~/
